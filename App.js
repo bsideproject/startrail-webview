d0d73254075b0eeb6506ac78e48cfee3417af098
devwoodie <ehddl453@naver.com>
2023-06-07 23:02:10 +0900

    feat : ios webview 구현

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React from 'react';
+import {
+    SafeAreaView,
+    StatusBar,
+    StyleSheet,
+    useColorScheme,
+    Alert,
+    BackHandler
+} from 'react-native';
+import MyWebView from './components/MyWebView';
+
+const App = () => {
+    const isDarkMode = useColorScheme() === 'dark';
+
+    return (
+        <>
+            <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+            <SafeAreaView style={styles.root}>
+                <MyWebView />
+            </SafeAreaView>
+        </>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    root: {
+        flex: 1
+    },
 });
+
+export default App;
